696eb32ac02df035c221695458b1fd2577645d18
debojitr <debojitr@amazon.com>
2023-02-16 11:02:27 +0530

    Initial commit

---
+++ test/e_kyc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EKyc from '../lib/e_kyc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/e_kyc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EKyc.EKycStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
