4dbcdb801e176c0ac097cb4a09678bdade02fb9c
debojitr <debojitr@amazon.com>
2023-02-27 16:49:28 +0530

    Added infra folder

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as EKyc from '../lib/e_kyc-stack';
+// import * as Infra from '../lib/infra-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/e_kyc-stack.ts
+// example resource in lib/infra-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new EKyc.EKycStack(app, 'MyTestStack');
+//   const stack = new Infra.InfraStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
