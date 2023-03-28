746a38008f6b847f0e4f46b64863060e0579466a
debojitr <debojitr@amazon.com>
2023-03-29 01:58:07 +0530

    Cleaned up unwanted files

---
+++ infra/src/api/lambdas/ekyc/status-check.ts
@@ -0,0 +1,5 @@
+import {APIGatewayProxyResultV2} from 'aws-lambda';
+import {
+    APIGatewayEventRequestContextJWTAuthorizer,
+    APIGatewayProxyEventBase
+} from "aws-lambda/trigger/api-gateway-proxy";
